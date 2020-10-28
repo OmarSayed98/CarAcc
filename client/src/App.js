041bf0eaaf76233337b4fd7737774b6e33ca4734
Omar El Halawany <omar.el-halawany@hotmail.com>
2020-10-28 22:17:13 +0200

    Tested sending request and receiving response from react to express

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class App extends Component {
   state = { 
     response:"",
    }
+
+  async componentDidMount(){
+
+    const {data} = await axios.get("http://localhost:3001/test");
+    
+    console.log(data);
+    this.setState({response:data})
+   }
+
+   async componentDidUpdate(){
+
+    const {data} = await axios.get("http://localhost:3001/test");
+    
+    this.setState({response:data});
+   }
+   
+
   render() { 
-    return ( <h1>Hello</h1> );
+  return ( <h1>{this.state.response}</h1> );
   }
 }
  
